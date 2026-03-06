5e000c821db05996a33f78cdc284f1ebdd1f8bf3
VolkovA <va771@yandex.ru>
2026-03-06 18:31:01 +0300

    Timestamps.fromMillis + StatusRuntimeException

--- stats/stats-client/src/main/java/ru/practicum/client/CollectorClient.java
+++ stats/stats-client/src/main/java/ru/practicum/client/CollectorClient.java
@@ -2,14 +2,14 @@ package ru.practicum.client;
 
 import com.google.protobuf.Empty;
 import com.google.protobuf.Timestamp;
+import com.google.protobuf.util.Timestamps;
+import io.grpc.StatusRuntimeException;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Component;
 import ru.practicum.stats.proto.ActionTypeProto;
 import ru.practicum.stats.proto.UserActionControllerGrpc;
 import ru.practicum.stats.proto.UserActionProto;
-
-import java.time.Instant;
 
 @Component
 @Slf4j
@@ -18,11 +18,7 @@ public class CollectorClient {
     private UserActionControllerGrpc.UserActionControllerBlockingStub client;
 
     public void collectUserAction(Long userId, Long eventId, ActionTypeProto action) {
-        Instant now = Instant.now();
-        Timestamp timestamp = Timestamp.newBuilder()
-                .setSeconds(now.getEpochSecond())
-                .setNanos(now.getNano())
-                .build();
+        Timestamp timestamp = Timestamps.fromMillis(System.currentTimeMillis());
 
         UserActionProto userActionProto = UserActionProto.newBuilder()
                 .setUserId(userId)
@@ -32,8 +28,8 @@ public class CollectorClient {
                 .build();
         try {
             Empty empty = client.collectUserAction(userActionProto);
-        } catch (Exception e) {
-            log.warn("Ошибка при вызове collectUserAction: {}.", e.getMessage());
+        } catch (StatusRuntimeException e) {
+            log.warn("Ошибка при вызове collectUserAction. \n Код статуса: {} \n Сообщение: {}", e.getStatus(), e.getMessage());
         }
     }
 }
